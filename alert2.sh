988636d777aa9626e7ecccde7bcae74171bf022d
malsieren222 <49137622+malsieren222@users.noreply.github.com>
2021-05-09 14:07:25 +0000

    Update alert2.sh

--- alert2.sh
+++ alert2.sh
@@ -4,12 +4,13 @@
 TuoIndirizzoEmail="<indirizzo-email>"
 Destinatario="<mail su cui ricevere notifica>"
 Oggetto=""
+CorpoMessaggio=""
 PATHFOLDER="lo stesso path usato su betaguard chiamato YOURMAINPATH"
 username="se hai marcobarca@gmail.com é marcobarca"
 password=""
 #modifica -s se non usi gmail per inviare email dallo script
 nohup sudo mpv $PATHFOLDER/alert.mp3 --no-video &
-sendemail -f $TuoIndirizzoEmail -t $Destinatario -u $Oggetto -m "ahahah" -a "/var/lib/motion/0"* -s smtp.googlemail.com:587 -xu $username -xp $password -o tls:yes
+sendemail -f $TuoIndirizzoEmail -t $Destinatario -u $Oggetto -m $CorpoMessaggio -a "/var/lib/motion/0"* -s smtp.googlemail.com:587 -xu $username -xp $password -o tls:yes
 wait
 sudo rm /var/lib/motion/*     
 ##ad ogni restart di funzione la folder media viene svuotata
